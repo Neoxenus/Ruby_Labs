5649ccaa07ef600db87c943746b0b619c18e4125
Volodymyr Kyryliuk <kirilyukvolodimir2003@gmail.com>
2022-09-08 22:27:20 +0300

    minor fix

--- lab1/main.rb
+++ lab1/main.rb
@@ -13,7 +13,7 @@ s = gets.to_i
 # print [a, b, c, d, x, s].join(' '), " \n"
 # puts 's value is mathematically incorrect' if s < 2
 begin 
-result = (Math.log(x**3)**2 + 0.375 * d) / (Math.exp(x**2) * Math.sqrt(c + a)) + (Math.tan(Math::PI / 6) - x**(3 / s)) / (0.316 * b * c - a**2).
+result = (Math.log(x**3)**2 + 0.375 * d) / (Math.exp(x**2) * Math.sqrt(c + a)) + (Math.tan(Math::PI / 6) - x**(3 / s)) / (0.316 * b * c - a**2).abs
 puts 'Result: ' + result.to_s
 rescue
 puts "Invalid data input"
